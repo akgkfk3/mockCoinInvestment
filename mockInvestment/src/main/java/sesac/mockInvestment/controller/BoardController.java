5052edcd3a7f5f458c8764c2caf5fb54d5ef49ca
sHu <akgkfk9@gmail.com>
2023-09-22 10:37:26 +0900

    feat: 게시판 뷰 페이지 개발

--- mockInvestment/src/main/java/sesac/mockInvestment/controller/BoardController.java
+++ mockInvestment/src/main/java/sesac/mockInvestment/controller/BoardController.java
@@ -1,8 +1,23 @@
 package sesac.mockInvestment.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class BoardController {
 
+    @GetMapping("/board")
+    public String test() {
+        return "board/coinInfo";
+    }
+
+    @GetMapping("/read")
+    public String test2() {
+        return "board/read";
+    }
+
+    @GetMapping("/write")
+    public String test3() {
+        return "board/write";
+    }
 }
